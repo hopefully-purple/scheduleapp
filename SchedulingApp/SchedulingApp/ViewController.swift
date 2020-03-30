f0b670cfee5b3c92c5926faff41c97b4c0da349e
hw <hopefully.purple@gmail.com>
2020-03-29 18:06:11 -0600

    Achieved text field functionality
    Temporarily, I have created a textfield that contains the name of the current schedule that can be changed by the user. Currently, it changes a seperate label. Next step is to get closer to what I actually want.

--- SchedulingApp/SchedulingApp/ViewController.swift
+++ SchedulingApp/SchedulingApp/ViewController.swift
@@ -18,21 +18,48 @@ import UIKit
  
  As of v0.1, this is named View Controller and the second view is named MainViewController. TODO: The naming needs to be fixed
  */
-class ViewController: UIViewController {
-  
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var name: String = ""
+    
+    @IBOutlet weak var scheduleNameL: UITextField!
+    
+    @IBOutlet weak var testLabel: UILabel!
     
     /**
      This is the viewDidLoad function for the Main View
      */
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        //Bit of code taken from https://stackoverflow.com/questions/11553396/how-to-add-an-action-on-uitextfield-return-key
+        self.scheduleNameL.addTarget(self, action: #selector(onReturn), for: UIControl.Event.editingDidEndOnExit)
+        
     }
     
-    //    var preferredDisplayMode: UISplitViewController.DisplayMode.primaryOverlay { get{} set{} }
-  
-//    var displayModeButtonItem: UIBarButtonItem { get{} }
+    /**
+     This function belongs to the text field that displays the name of the current schedule.
+     User can tap the field and change the name of the schedule.
+     The basic default name is "Me", and if there are other people added, the default is "Me and <name>"
+     
+     User should be able to either hit return or tap outside of the field to make it save and for the keyboard to disappear.
+     
+        Need to grab the input and update the Model
+        Need to display corresponding default text at first
+     
+     TODO: Determine if text field is the right object.
+     
+     The function declaration and the first line of code is taken from https://stackoverflow.com/questions/11553396/how-to-add-an-action-on-uitextfield-return-key
+     */
+   @IBAction func onReturn() {
+    //scheduleNameL is no longer the first responder
+    self.scheduleNameL.resignFirstResponder()
+
+    //Change the label name to the input. TODO: Delete this temp feature
+    name = scheduleNameL.text!
+    testLabel.text = name
+   }
+    
     
 }
 
